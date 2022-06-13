c43cd3d329985aab389fbfa05705b9a83477aee3
Cao Ruijuan <caoruijuan@dengdengli.com>
2022-06-13 19:21:45 +0800

    read and write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use xsk_rs::umem;
+
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut f = File::open("test.txt").await?;
+    let mut buffer = [0; 10];
+
+    // read up to 10 bytes
+    let n = f.read(&mut buffer[..]).await?;
+
+    println!("The bytes: {:?}", &buffer[..n]);
+    Ok(())
 }
