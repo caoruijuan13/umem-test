e86629377ae6642420b915652b782597c856976f
Cao Ruijuan <caoruijuan@dengdengli.com>
2022-06-14 12:36:59 +0800

    test umem read and write

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,72 @@
-use xsk_rs::umem;
-
-use tokio::fs::File;
+use std::vec;
+use std::{convert::TryInto, io::Write, str};
+use std::collections::VecDeque;
+use xsk_rs::{config::UmemConfig, umem::Umem};
 use tokio::io::{self, AsyncReadExt};
 
-async fn test() {
-    let frame_count = 64;
 
-    let (umem, descs) = Umem::new(
-        UmemConfig::default(),
+async fn umem_test() {
+    // config
+    let frame_size = 2048;
+    let frame_count = 64;
+    let frame_headroom = 32;
+
+    // init
+    let (umem, mut descs) = Umem::new(
+        UmemConfig::builder()
+        .frame_headroom(frame_headroom)
+        .frame_size(frame_size).build().unwrap(),
         frame_count.try_into().unwrap(),
         false,
     )
     .unwrap();
 
-}
+    // queue
+    // let q: VecDeque<u32> = VecDeque::with_capacity(10);
+    let q: VecDeque<u32> = VecDeque::new();
+    for i in 0..frame_count {
+        q.push_back(i);
+    }
 
-async fn frame_test() {
-    let (umem, mut descs) = Umem::new(
-        UmemConfig::builder().frame_headroom(32).build().unwrap(),
-        64.try_into().unwrap(),
-        false,
-    )
-    .unwrap();
+    // get ownership
+    let write_num = 3;
+    for i in 0..write_num {
+        let index = q.pop_back();
+        // write and assert
+        unsafe {
+            let (mut h, mut d) = umem.frame_mut(&mut descs[index]);
 
-    unsafe {
-        let (mut h, mut d) = umem.frame_mut(&mut descs[0]);
+            h.cursor().write_all(b"hello").unwrap();
+            h.cursor().write_all(index).unwrap();
+            d.cursor().write_all(b"world").unwrap();
+            d.cursor().write_all(index).unwrap();
 
-        h.cursor().write_all(b"hello").unwrap();
-        d.cursor().write_all(b"world").unwrap();
+            println!("descs: {:?}", descs);
+            println!("headroom-{}: {:?}", index, umem.headroom(&descs[index]).contents());
+            println!("data-{}: {:?}", index, umem.data(&descs[index]).contents());
 
-        println!("descs: {:?}", descs);
+            // assert_eq!(umem.headroom(&descs[index]).contents(), b"hello");
+            // assert_eq!(umem.headroom_mut(&mut descs[index]).contents(), b"hello");
 
-        assert_eq!(umem.headroom(&descs[0]).contents(), b"hello");
-        assert_eq!(umem.headroom_mut(&mut descs[0]).contents(), b"hello");
+            // assert_eq!(umem.data(&descs[index]).contents(), b"world");
+            // assert_eq!(umem.data_mut(&mut descs[index]).contents(), b"world");
+        }
+    }
 
-        assert_eq!(umem.data(&descs[0]).contents(), b"world");
-        assert_eq!(umem.data_mut(&mut descs[0]).contents(), b"world");
+    // read
+    let rx_q = vec![0, 2];
+    for index in rx_q {
+        unsafe {
+            println!("descs: {:?}", descs);
+            println!("headroom-{}: {:?}", index, umem.headroom(&descs[index]).contents());
+            println!("data-{}: {:?}", index, umem.data(&descs[index]).contents());
+        }
     }
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    // let mut f = File::open("test.txt").await?;
-    // let mut buffer = [0; 10];
-
-    // // read up to 10 bytes
-    // let n = f.read(&mut buffer[..]).await?;
-
-    // println!("The bytes: {:?}", &buffer[..n]);
-
-    frame_test().await;
+    umem_test().await;
 
     Ok(())
 }
