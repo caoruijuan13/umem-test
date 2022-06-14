91105619fb8a37d0756c2ec1654b2e9b53ca602a
caoruijuan13@gmail.com <caoruijuan13@gmail.com>
2022-06-14 13:25:06 +0800

    refactor read and write

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use std::vec;
 use std::{convert::TryInto, io::Write, str};
 use std::collections::VecDeque;
-use xsk_rs::{config::UmemConfig, umem::Umem};
+use xsk_rs::{config::{FrameSize, UmemConfig}, umem::Umem};
 use tokio::io::{self, AsyncReadExt};
 
 
 async fn umem_test() {
     // config
     let frame_size = 2048;
-    let frame_count = 64;
+    let frame_count = 3;
     let frame_headroom = 32;
 
     // init
     let (umem, mut descs) = Umem::new(
         UmemConfig::builder()
         .frame_headroom(frame_headroom)
-        .frame_size(frame_size).build().unwrap(),
+        .frame_size(FrameSize::new(frame_size).unwrap()).build().unwrap(),
         frame_count.try_into().unwrap(),
         false,
     )
@@ -23,7 +23,7 @@ async fn umem_test() {
 
     // queue
     // let q: VecDeque<u32> = VecDeque::with_capacity(10);
-    let q: VecDeque<u32> = VecDeque::new();
+    let mut q: VecDeque<u32> = VecDeque::new();
     for i in 0..frame_count {
         q.push_back(i);
     }
@@ -31,19 +31,22 @@ async fn umem_test() {
     // get ownership
     let write_num = 3;
     for i in 0..write_num {
-        let index = q.pop_back();
+        let index = q.pop_front().unwrap() as usize;
         // write and assert
         unsafe {
             let (mut h, mut d) = umem.frame_mut(&mut descs[index]);
 
-            h.cursor().write_all(b"hello").unwrap();
-            h.cursor().write_all(index).unwrap();
-            d.cursor().write_all(b"world").unwrap();
-            d.cursor().write_all(index).unwrap();
+            //let index_bytes = (1000+index).to_le_bytes();
+            //println!("index = {}, bytes = {:?}", index, index_bytes);
+            
+            h.cursor().write_all(format!("hello-{}", index).as_bytes()).unwrap();
+            //h.cursor().write_all(&index_bytes).unwrap();
+            d.cursor().write_all(format!("world-{}", index).as_bytes()).unwrap();
+            //d.cursor().write_all(&index_bytes).unwrap();
 
             println!("descs: {:?}", descs);
-            println!("headroom-{}: {:?}", index, umem.headroom(&descs[index]).contents());
-            println!("data-{}: {:?}", index, umem.data(&descs[index]).contents());
+            println!("headroom-{}: {:?}", index, str::from_utf8(umem.headroom(&descs[index]).contents()));
+            println!("data-{}: {:?}", index, str::from_utf8(umem.data(&descs[index]).contents()));
 
             // assert_eq!(umem.headroom(&descs[index]).contents(), b"hello");
             // assert_eq!(umem.headroom_mut(&mut descs[index]).contents(), b"hello");
